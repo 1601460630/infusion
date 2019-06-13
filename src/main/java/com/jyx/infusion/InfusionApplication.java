c29b7a6ea04dd0071560e88c8e6b58c8e3134ead
1601460630 <jyx1601460630@outlook.com>
2019-06-13 16:28:42 +0800

    V0.1

--- src/main/java/com/jyx/infusion/InfusionApplication.java
+++ src/main/java/com/jyx/infusion/InfusionApplication.java
@@ -3,11 +3,15 @@ package com.jyx.infusion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author jyxlalala
+ * @Date 2019/6/13 16:28
+ */
 @SpringBootApplication
 public class InfusionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InfusionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InfusionApplication.class, args);
+    }
 
 }
