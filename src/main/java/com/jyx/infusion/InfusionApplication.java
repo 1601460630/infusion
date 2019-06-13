6da7f2b2d088cb6f112f309d8c81c36e0f1846a4
1601460630 <jyx1601460630@outlook.com>
2019-06-13 16:24:18 +0800

    V0.1

---
+++ src/main/java/com/jyx/infusion/InfusionApplication.java
@@ -0,0 +1,13 @@
+package com.jyx.infusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfusionApplication.class, args);
+	}
+
+}
